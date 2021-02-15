a055119ca173a1db466ed947b8117642678870ce
emonzon42 <elijah.monzon42@gmail.com>
2021-02-15 13:18:17 -0500

    added and/or/xor/not/tostring

--- Homework/HW2/Longword.java
+++ Homework/HW2/Longword.java
@@ -1,9 +1,14 @@
 public class Longword {
     
-    Bit[] bits;
+    public final int LONGWORD_SIZE = 32;
+    private Bit[] bits;
 
     public Longword(){
-        bits = new Bit[32];
+        bits = new Bit[LONGWORD_SIZE];
+        for (int i = 0; i < LONGWORD_SIZE; i++) {
+            bits[i] = new Bit();
+              //  System.out.println(bit.getValue());
+        }
     }
 
     public Bit getBit(int i){  // Get bit i
@@ -14,5 +19,50 @@ public class Longword {
         bits[i].set(value.getValue());
     }
 
+    public Longword and(Longword b){
+        Longword a = new Longword();
+        for (int i = 0; i < LONGWORD_SIZE; i++) {
+            a.setBit(i, bits[i].and(b.getBit(i)));
+        }
+        return a;
+    }
+    
+    public Longword or(Longword b){
+        Longword a = new Longword();
+        for (int i = 0; i < LONGWORD_SIZE; i++) {
+            a.setBit(i, bits[i].or(b.getBit(i)));
+        }
+        return a;
+    }
 
+    public Longword xor(Longword b){
+        Longword a = new Longword();
+        for (int i = 0; i < LONGWORD_SIZE; i++) {
+            a.setBit(i, bits[i].xor(b.getBit(i)));
+        }
+        return a;
+    }
+    
+    public Longword not(){
+        Longword a = new Longword();
+        for (int i = 0; i < LONGWORD_SIZE; i++) {
+            a.setBit(i, bits[i].not());
+        }
+        return a;
+    }
+
+    //! Longword rightShift(int amount); // rightshift this longword by amount bits, creating a new longword    
+    //! Longword leftShift(int amount); // leftshift this longword by amount bits, creating a new longword
+
+    @Override 
+    public String toString(){ // returns a comma separated string of 0's and 1's: "0,0,0,0,0 (etcetera)" for example
+        StringBuilder str = new StringBuilder();
+        for (Bit bit : bits) {
+            str.append(bit.getValue());
+            
+            if (str.length() != 63) //63 = 32 bits + 31 commas
+                str.append(",");
+        }
+        return str.toString();
+    }
 }
---
+++ Homework/HW2/longword_test.java
@@ -0,0 +1,6 @@
+public class longword_test {
+    public static void main(String[] args) {
+        Longword l = new Longword();
+        System.out.println(l);
+    }
+}
