0dbbd35154240cd5353913aa84c97d6577f70c79
emonzon42 <elijah.monzon42@gmail.com>
2021-02-24 19:17:04 -0500

    implemented subtract() and testsub()

--- Homework/HW3/RippleAdder.java
+++ Homework/HW3/RippleAdder.java
@@ -1,28 +1,91 @@
 public class RippleAdder {
-    public static Longword add(Longword a, Longword b){
+    public static Longword add(Longword a, Longword b){ //takes two longwords and adds them together
+        if(a.getSigned() < 0) //if one of the longwords is negative
+            return subtract(b, twosComplement(a));
+        else if(b.getSigned() < 0)
+            return subtract(a, twosComplement(b));
+
+     //   System.out.println(a.getSigned() + " + " + b.getSigned());
         Longword c = new Longword();
         int carry = 0;
         for (int i = c.LONGWORD_SIZE-1  ; i > 0; i--) {
             int add =  a.getBit(i).getValue() + b.getBit(i).getValue() + carry;
-            
+            //System.out.println();
+            //System.out.print(a + "\n+\n" + b + "\n--------------------------------------------------------------------");
             if (carry == 0 && add > 1){ // 1 + 1 + 0(carry)
                 c.setBit(i, 0);
                 carry++;
-            } else if (carry > 0 && add > 2){ // 1 + 1 + 1(carry)
+            }else if (carry > 0 && add > 2){ // 1 + 0 + 1(carry)
                 c.setBit(i, 1);
-            }else if (carry > 1)  { // 0 + 0 + (carry > 1)  or 1 + 0 + (carry > 1)
+            }else if (carry > 0 && add > 1){ // 1 + 1 + 1(carry)
+                c.setBit(i, 0);
+            }else if (carry > 0)  { // 0 + 0 + (carry > 1)  or 1 + 0 + (carry > 1)
                 c.setBit(i, add - carry + 1);
                 carry--;
-            } else { // 0 + 0 + 1(carry) or 1 + 0 + 1(carry)
+            } else { // 0 + 0 + 1(carry) or 1 + 0 + 1(carry) or 1 + 0 + 0(carry)
                 c.setBit(i, add);
                 if (carry != 0)
                     carry--;
             }
 
-           //// System.out.println();
-          ////  System.out.println(a + "\n+\n" + b + " = " + c + " - carry: " + carry);
+            //System.out.println();
+            //System.out.println(c);
         }
-
+     //   System.out.println(c.getSigned());
         return c;
     }
+
+    public static Longword subtract(Longword a, Longword b){ //takes two longword and subtracts one from the other
+        if(a.getSigned() < 0 && b.getSigned() < 0)
+            return subtract(twosComplement(b), twosComplement(a));
+
+
+        Longword aa = new Longword(a); //backups a because a can get modified during subtraction
+    //    System.out.println();
+     //   System.out.println(a.getSigned() + " - " + b.getSigned());
+        Longword c = new Longword();
+        for (int i = c.LONGWORD_SIZE-1  ; i >= 0; i--) {
+         //   System.out.println();
+        //    System.out.println();
+        //    System.out.print(a + "\n-\n" + b + "\n--------------------------------------------------------------------");
+            int sub =  a.getBit(i).getValue() - b.getBit(i).getValue();
+            //* https://www.youtube.com/watch?v=h_fY-zSiMtY
+            if (sub < 0){ //0 - 1
+            //    System.out.print(" = " + sub);
+                int borrow = borrow(i, a); //i tried skipping this step to save mem but it forces me to save the value 
+                sub = a.getBit(i).getValue() - b.getBit(i).getValue() + borrow; //should now be 1 - 1 + 1
+              //  System.out.print("  : " + sub + " --: "+ borrow);
+            }
+        //    System.out.println();
+            c.setBit(i, sub);
+        //    System.out.println(c);
+          //  System.out.println(a + "\n-\n" + b + " = " + c);
+        }
+      //  System.out.println(c.getSigned());
+        a = aa; //sets a back to what it started as
+        return c;
+    }
+
+    public static Longword sub(Longword a, Longword b){ //subtract with a shorter method name
+        return subtract(a, b);
+    }
+
+    public static Longword twosComplement(Longword a){ // returns the two's complement of a
+        return add(a.not(),new Longword(1));
+    }
+
+    private static int borrow(int index, Longword a){ //recursively borrows a 1 from the nearest bit = 1 
+        if(index == 0){
+            a.setBit(index, 1);
+            return 1;
+        }else if (a.getBit(index - 1).getValue() == 1){
+            a.setBit(index - 1, 0);
+            a.setBit(index, 1);
+            return 1;
+        }else{
+            a.setBit(index, borrow(index - 1, a));
+            return 1;
+        }
+    }
+    
 }
--- Homework/HW3/rippleAdder_test.java
+++ Homework/HW3/rippleAdder_test.java
@@ -21,15 +21,24 @@ public class rippleAdder_test {
 
     public static void testAdd() throws Exception{
         if (RippleAdder.add(new Longword(1), new Longword(1)).getSigned() != 2) throw new Exception("Add failed @ 1 + 1 = 2");
-        if (RippleAdder.add(new Longword(1), new Longword(2)).getSigned() != 3) throw new Exception("Add failed @ 1 + 2 = 3");
+        if (RippleAdder.add(new Longword(23), new Longword(1)).getSigned() != 24) throw new Exception("Add failed @ 23 + 1 = 24");
         if (RippleAdder.add(new Longword(50), new Longword(50)).getSigned() != 100) {
-            System.out.println(RippleAdder.add(new Longword(50), new Longword(50)).getSigned());
+            //System.out.println(RippleAdder.add(new Longword(50), new Longword(50)).getSigned());
             throw new Exception("Add failed @ 50 + 50 = 100");}
-        //if (RippleAdder.add(new Longword(-50), new Longword(80)).getSigned() != 30) throw new Exception("Add failed @ -50 + 80 = 70");
+        if (RippleAdder.add(new Longword(-50), new Longword(80)).getSigned() != 30) throw new Exception("Add failed @ -50 + 80 = 30");
+        if (RippleAdder.add(new Longword(162), new Longword(-54)).getSigned() != 108) throw new Exception("Add failed @ 162 + -54 = 108");
+        if (RippleAdder.add(new Longword(-653), new Longword(-47)).getSigned() != -700) throw new Exception("Add failed @ -653 + -44 = -700");
     }
 
     public static void testSub() throws Exception{
-        
-        
+        if (RippleAdder.subtract(new Longword(1), new Longword(1)).getSigned() != 0) throw new Exception("Sub failed @ 1 - 1 = 0");
+        if (RippleAdder.subtract(new Longword(14), new Longword(5)).getSigned() != 9) {
+           // System.out.println(new Longword(14));
+            //System.out.println(new Longword(5));
+            //System.out.println(RippleAdder.subtract(new Longword(14), new Longword(5)));
+            throw new Exception("Sub failed @ 14 - 5 = 9");}
+        if (RippleAdder.subtract(new Longword(1), new Longword(-1)).getSigned() != 2) throw new Exception("Sub failed @ 1 - -1 = 2");
+        if (RippleAdder.subtract(new Longword(-26), new Longword(-24)).getSigned() != -2) throw new Exception("Sub failed @ -26 - -24 = -2");
+        if (RippleAdder.subtract(new Longword(-50), new Longword(5)).getSigned() != -55) throw new Exception("Sub failed @ -50 - 5 = -55");
     }
 }
