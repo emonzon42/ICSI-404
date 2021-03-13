b791a4b8f82340ed4a0a325231981c6b7f9cf574
emonzon42 <elijah.monzon42@gmail.com>
2021-03-12 20:02:31 -0500

    started hw6: implemented pt1 & pt2

--- Homework/HW5/Computer.java
+++ Homework/HW5/Computer.java
@@ -2,10 +2,19 @@ public class Computer {
     
     public Bit onoff; // bit to represent whether computer is on or off
     private Memory mem; //computer memory
+    private Longword PC; //program counter
+    private Longword[] R; //the registers
+
+    private Longword currentInstruction;
+    private Longword op1, op2;
 
     public Computer(){ //constructor
         mem = new Memory();
         onoff = new Bit(0);
+        PC = new Longword();
+        R = new Longword[16];
+        for (int i = 0; i < R.length; i++)
+            R[i] = new Longword();
     }
 
     public void run(){
@@ -18,11 +27,12 @@ public class Computer {
     }
 
     public void fetch(){
-
+        currentInstruction = mem.read(PC);
+        PC = PC.plus(new Longword(2)); //increments PC by 2
     }
 
     public void decode(){
-        
+        //TODO: examine currentInstruction to determine 2 registers by shifting it around
     }
 
     public void execute(){
