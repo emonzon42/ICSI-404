571820f3d78ef285cfc262a33b42eec70e9c0c4e
emonzon42 <elijah.monzon42@gmail.com>
2021-03-25 23:01:05 -0400

    updated documentation & made bit comparable

--- Homework/HW1/Bit.java
+++ Homework/HW1/Bit.java
@@ -1,63 +1,63 @@
-public class Bit implements IBit {
+public class Bit implements IBit, Comparable<Bit> {
 
     private byte bit = 0;
 
-    public Bit(int value) {
+    public Bit(int value) { //constructor, sets bit to 1 unless value is 0
         set(value);
     }
 
-    public Bit() {
+    public Bit() { //default constructor, sets bit to 0
         this(0);
     }
 
-    public void set(int value) {
+    public void set(int value) { //sets value of bit
         if (value != 0)
             bit = 1;
         else
             bit = 0;
     }
 
-    public void set() {
+    public void set() { //sets value of bit to 1
         set(1);
     }
 
-    public void clear() {
+    public void clear() { //clears value of bit
         set(0);
     }
 
-    public void toggle() {
+    public void toggle() { //toggles value of bit
         if (bit == 1)
             clear();
         else
             set();
     }
 
-    public int getValue(){
+    public int getValue(){ //returns value of bit
         return bit;
     }
 
-    public Bit and(Bit b){
+    public Bit and(Bit b){ //returns the result of this bit AND b
         if (bit == 1 && b.getValue() == 1)
             return new Bit(1);
         else
             return new Bit(0);
     }
 
-    public Bit or(Bit b){
+    public Bit or(Bit b){ //returns the result of this bit OR b
         if (bit == 0 && b.getValue() == 0)
             return new Bit(0);
         else
             return new Bit(1);
     }
 
-    public Bit xor(Bit b){
+    public Bit xor(Bit b){ //returns the result of this bit XOR b
         if ((bit == 1 && b.getValue() == 0 )|| (bit == 0 && b.getValue() == 1))
             return new Bit(1);
         else
             return new Bit(0);
     }
 
-    public Bit not(){
+    public Bit not(){ //returns the value of this value NOT
         if (bit == 0)
             return new Bit(1);
         else
@@ -67,6 +67,20 @@ public class Bit implements IBit {
     @Override 
     public String toString(){
         return Integer.toString(bit);
+    }
+
+    @Override
+    public int compareTo(Bit b){
+        if (bit == b.getValue())
+            return 0;
+        else if (bit < b.getValue())
+            return -1;
+        else
+            return 1;
+    }
+
+    public boolean equals(Bit b){ //checks whether this and b are equal
+        return bit == b.getValue() ? true : false;
     }
 
 }
--- Homework/HW4/ALU.java
+++ Homework/HW4/ALU.java
@@ -3,23 +3,23 @@ public class ALU {
         if (b == null)
             return doSoloOp(operation,a);
 
-        if (areEqual(operation, new Bit[]{new Bit(1),new Bit(0),new Bit(0),new Bit(0)})){ //operation = 1000
+        if (areEqual(operation, new Bit[]{new Bit(1),new Bit(0),new Bit(0),new Bit(0)})){ //1000
             return a.and(b);
-        }else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(0),new Bit(0),new Bit(1)}))
+        }else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(0),new Bit(0),new Bit(1)}))//1010
             return a.or(b);
-        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(0),new Bit(1),new Bit(0)}))
+        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(0),new Bit(1),new Bit(0)})) //1010
             return a.xor(b);
-        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(0),new Bit(1),new Bit(1)}))
+        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(0),new Bit(1),new Bit(1)})) //1011
             return a.not();
-        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(1),new Bit(0),new Bit(0)}))
+        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(1),new Bit(0),new Bit(0)})) //1100
             return a.leftShift(1);
-        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(1),new Bit(0),new Bit(1)}))
+        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(1),new Bit(0),new Bit(1)})) //1101
             return a.rightShift(1);
-        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(1),new Bit(1),new Bit(0)}))
+        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(1),new Bit(1),new Bit(0)})) //1110
             return a.plus(b);
-        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(1),new Bit(1),new Bit(1)}))
+        else if (areEqual(operation, new Bit[]{new Bit(1),new Bit(1),new Bit(1),new Bit(1)})) //1111
             return a.minus(b);
-        else if (areEqual(operation, new Bit[]{new Bit(0),new Bit(1),new Bit(1),new Bit(1)}))
+        else if (areEqual(operation, new Bit[]{new Bit(0),new Bit(1),new Bit(1),new Bit(1)})) //0111
             return a.times(b);
         
         return new Longword();
@@ -36,7 +36,7 @@ public class ALU {
         return new Longword();
     }
 
-    private static boolean areEqual(Bit[] a, Bit[] b){ //Bit[] adaptation of Arrays.equals() method
+    public static boolean areEqual(Bit[] a, Bit[] b){ //Bit[] adaptation of Arrays.equals() method
         if (a==b)
             return true;
         if (a==null || b==null)
@@ -46,7 +46,7 @@ public class ALU {
             return false;
         
         for (int i = 0; i < b.length; i++) {
-            if(a[i] == null || b[i] == null || a[i].getValue() != b[i].getValue())
+            if(a[i] == null || b[i] == null || !a[i].equals(b[i]))
                 return false;
         }
 
