602772bcb595fb9071d436eedb37694ae69b9c5d
emonzon42 <elijah.monzon42@gmail.com>
2021-04-07 01:06:41 -0400

    memory is now read/written in 16 bit blocks

--- Homework/HW5/Memory.java
+++ Homework/HW5/Memory.java
@@ -9,16 +9,16 @@ public class Memory {
             mem[i] = new Bit(0);
     }
 
-    public Longword read(Longword address){ //reads the bits of the memory starting at mem[address] into a new longword
+    public Longword read(Longword address){ //reads the bits of the memory starting at mem[16*address] into a new longword
         Longword data = new Longword();
-        for (int i = 0, j = address.getSigned(); i < data.LONGWORD_SIZE && j < MEM_SIZE; i++, j++) {
+        for (int i = 0, j = address.getSigned()*16; i < data.LONGWORD_SIZE && j < MEM_SIZE; i++, j++) {
             data.setBit(i, mem[j]);
         }
         return data;
     }
 
-    public void write(Longword address, Longword value){ //writes value into memory starting at mem[address]
-        for (int i = address.getSigned(), j = 0; i < MEM_SIZE && j< value.LONGWORD_SIZE; i++, j++) {
+    public void write(Longword address, Longword value){ //writes value into memory starting at mem[16*address]
+        for (int i = address.getSigned()*16, j = 0; i < MEM_SIZE && j< value.LONGWORD_SIZE; i++, j++) {
             mem[i] = value.getBit(j);
         }
     }
